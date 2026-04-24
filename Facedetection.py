3bc476818edfd629d721c9a54d2bc6bbd7673cbf
bandikanishk <bandikanishk@gmail.com>
2026-04-24 15:35:09 -0400

    Refactor face detection code for live video

--- Facedetection.py
+++ Facedetection.py
@@ -5,18 +5,8 @@ from utils import rescaleFrame
 """
 Haar cascade is an algorithim thats already been made to detect images and is trained on images already
 """
-#detect faces in images
-img = cv.imread('pics/fast.jpg')
-resized = rescaleFrame(img,0.25)
-gray = cv.cvtColor(resized,cv.COLOR_BGR2GRAY) #doesnt look at skin tone only edges
-haar_cascade = cv.CascadeClassifier('haar_face.xml')
-smile_cascade = cv.CascadeClassifier('haar_smile.xml')
-faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5) #returns a list of potential faces
 
-for (x,y,w,h) in faces_rect:
-    cv.rectangle(resized,(x,y),(x+w,y+h),(0,255,0),5)
-cv.imshow('img',resized)
-cv.waitKey(0)
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
 # detect faces in live video
 capture = cv.VideoCapture(0)
 while True:
